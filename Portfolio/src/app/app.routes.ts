0d65b082e6b4ef380c364d1e9fa9551e7b79a37e
unknown <39144312Ff>
2024-08-14 01:43:07 +0300

    update

--- Portfolio/src/app/app.routes.ts
+++ Portfolio/src/app/app.routes.ts
@@ -5,6 +5,6 @@ export const routes: Routes = [
     {path: 'Home', loadComponent: () => import ('../app/components/portbody/portbody.component').then(h=>h.PortbodyComponent) },
     {path: 'about', loadComponent: ()=> import ('../app/components/about/about.component').then(a=>a.AboutComponent)},
     {path: 'projects', loadComponent: () => import ('./projects/projects.component').then(p=>p.ProjectsComponent)},
-    {path: 'services', loadComponent: ()=> import ('../app/components/services/services.component').then(s=>s.ServicesComponent)},
-    {path:'contact', loadComponent: ()=> import ('../app/components/contact/contact.component').then(c=>c.ContactComponent)} 
+    {path:'contact', loadComponent: ()=> import ('../app/components/contact/contact.component').then(c=>c.ContactComponent)},
+    {path: 'services', loadComponent: ()=> import('../app/components/services/services.component').then(s=>s.ServicesComponent)}
 ];
